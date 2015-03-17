dbf0fa17cd561c238263827fe7bd4ca56b2c5aa3
Veronika <veronika.prok@yandex.ru>
2015-03-18 02:31:32 +0300

    Point and Curve classes created

---
+++ FontEditor/Model/Curve.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FontEditor.Model
+{
+    class Curve
+    {
+        public Point[] ControlPoints; // Used to create the curve
+        public Point[] AllPoints; // Used to draw the curve
+
+        public Curve(Point[] controlPoints)
+        {
+            ControlPoints = controlPoints;
+            MagicCurveCreation();
+        }
+        
+        // TODO implement MagicCurveCreation()
+        private void MagicCurveCreation()
+        {
+            // TODO using ContolPoints initialize AllPoints
+        }
+    }
+}
