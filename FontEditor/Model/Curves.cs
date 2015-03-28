c966f75a9cbcd8fd8e64de23c68a5f73f8e85374
Veronika <veronika.prok@yandex.ru>
2015-03-29 02:33:43 +0300

    Old structure (will be completely replaced with a new one)

---
+++ FontEditor/Model/Curves.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms.VisualStyles;
+
+namespace FontEditor.Model
+{
+    public class Curves
+    {
+        private List<Curve> _Curves = new List<Curve>();
+        private Curve _CurrentCurve;
+
+        public void AddCurve(List<Point> points)
+        {
+            _Curves.Add(new Curve(){ Points = points });
+        }
+
+        public List<Point> FindCurve(List<Point> points)
+        {
+            var curveToBeFound = new Curve(){ Points = points };
+            _CurrentCurve = _Curves.Find(curve => Equals(curve, curveToBeFound));
+           
+            return _CurrentCurve != null ? _CurrentCurve.Points : null;  // returned points are used for drawing purposes
+        }
+
+        // Find curve point 'from' and move it to 'to'
+        public void MoveCurvePoint(Point from, Point to)
+        {
+            // change 'from' point of _CurrentCurve
+            _CurrentCurve.MovePoint(from, to);
+            // TODO update _Curves
+        }
+
+        public void RemoveCurve()
+        {
+            // delete _CurrentCurve from _Curves
+        }
+    }
+
+    internal class Curve
+    {
+        // 4 points of the curve
+        public List<Point> Points;
+
+        public override bool Equals(object curve2)
+        {
+            return AreAllClosePoints(Points, ((Curve) curve2).Points);
+        }
+
+        public void MovePoint(Point from, Point to)
+        {
+            const int tolerance = 3; // tolerance in pixels
+
+            // first find 'from' point in the list of the curve points
+            for (int i = 0; i < Points.Count; i++)
+            {
+                if (Points[i].X >= from.X - tolerance && Points[i].X <= from.X + tolerance
+                    && Points[i].Y >= from.Y - tolerance && Points[i].Y <= from.Y + tolerance)
+                {
+                    var point = Points[i];
+                    point.X = to.X;
+                    point.Y = to.Y;
+                }
+            }
+
+        }
+
+        private bool AreAllClosePoints(List<Point> curve1Points, List<Point> curve2Points)
+        {
+            return curve1Points.All(point1 => ExistsClosePoint(point1, curve2Points));
+        }
+
+        private bool ExistsClosePoint(Point point, List<Point> points)
+        {
+            const int tolerance = 3; // tolerance in pixels
+            return points.Any(_point => point.X >= _point.X - tolerance && point.X <= _point.X + tolerance
+                                        && point.Y >= _point.Y - tolerance && point.Y <= _point.Y + tolerance);
+        }
+    }
+}
--- FontEditor/View/MainWindow.xaml.cs
+++ FontEditor/View/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.Collections;
+using System.Windows.Media.Animation;
+using FontEditor.Model;
 
 namespace FontEditor
 {
@@ -31,27 +33,69 @@ namespace FontEditor
             FONT_EDIT,
             TEXT_EDIT
         };
-        private FontEditor.Model.Curve[] currentCurves;
-        private Stack<Point> currentPoints; // all points on current frame; each sequential quad of them gives us a curve
-                 // we nned to store points in order to be able to remove recently drawn
+        
         private Tabs m_currentTab = Tabs.FONT_EDIT;
 
+        private Curves _Curves;
+
+        private Stack<Point> _CurrentPoints = new Stack<Point>();  // used to add and to remove points while drawing
 
         public MainWindow()
         {
             InitializeComponent();
+
+            //CreateBezierSegment();
+
         }
+
+        private void CreateBezierSegment()
+        {
+            var bezierSegment = new BezierSegment(new Point(100, 200), new Point(400, 200), new Point(400, 300), true);
+            var pathSegmentCollection = new PathSegmentCollection {bezierSegment};
+
+            var pathFigure = new PathFigure {StartPoint = new Point(100, 300), Segments = pathSegmentCollection};
+            var pathFigureCollection = new PathFigureCollection {pathFigure};
+
+            var pathGeometry = new PathGeometry {Figures = pathFigureCollection};
+
+            var path = new Path {Stroke = new SolidColorBrush(Colors.Black), Data = pathGeometry};
+
+            fontCanvas.Children.Add(path);
+        }
+
 
         private void fontCanvas_MouseLeftButtonDown(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            System.Windows.MessageBox.Show("You have clicked canvas", "My Application");
-
             // understand whether user clicked on already existing point or he created new point
+            
+            _CurrentPoints.Push(e.GetPosition(fontCanvas));
+            DrawPoint();
+
+            if (_CurrentPoints.Count == 4)
+            {
+                _Curves.AddCurve(_CurrentPoints.ToList());
+                DrawCurve(_CurrentPoints.ToList());
+            }
         }
+
+        private void DrawCurve(List<Point> points)
+        {
+             // draw Bezier segment
+        }
+
+        private void DrawPoint()
+        {
+                // draw point on canvas
+            var point = new Ellipse();
+
+
+
+        }
+
 
         private void fontCanvas_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            System.Windows.MessageBox.Show("You are moving mouse", "My Application");
+           // System.Windows.MessageBox.Show("You are moving mouse", "My Application");
 
             // if point was clicked or just has been created, we pull this point
         }
